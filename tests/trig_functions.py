b13ac435293393d4f0b7e3069515385de5ddae74
acse-va220 <vyronavramidis@gmail.com>
2020-10-21 18:47:04 +0100

    Changed styling v7.0

--- tests/trig_functions.py
+++ tests/trig_functions.py
@@ -6,6 +6,6 @@ __all__ = ['sin']
 def sin(x, n):
 
     sumi = sum(((-1)**i * x**(2*i + 1))/(factorial(2*i + 1))
-            for i in range(n+1))
+               for i in range(n+1))
 
     return sumi
